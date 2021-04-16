48e59e748d4ba67f300407a77a400732a4719cee
Andrew <blakexiaolong36@gmail.com>
2021-04-16 01:30:46 -0400

    add more fields to database

---
+++ db/migrate/20210415230434_add_form_fields_to_instance_entry.rb
@@ -0,0 +1,24 @@
+class AddFormFieldsToInstanceEntry < ActiveRecord::Migration[6.1]
+  def change
+    add_column :instance_entries, :start_time, :datetime
+    add_column :instance_entries, :roulette_name, :string
+    add_column :instance_entries, :job_name, :string
+    add_column :instance_entries, :start_level, :number
+    add_column :instance_entries, :start_xp, :number
+    add_column :instance_entries, :queue_pop_time, :datetime
+    add_column :instance_entries, :instance_selection, :string
+    add_column :instance_entries, :finish_time, :datetime
+    add_column :instance_entries, :finish_level, :number
+    add_column :instance_entries, :finish_xp, :number
+    add_column :instance_entries, :xp_bonus, :number
+    add_column :instance_entries, :roulette_bonus, :number
+    add_column :instance_entries, :new_player_bonus, :number
+    add_column :instance_entries, :role_in_need_bonus, :number
+    add_column :instance_entries, :other_bonus, :number
+    add_column :instance_entries, :commends, :number
+    add_column :instance_entries, :queue_outlier?, :boolean
+    add_column :instance_entries, :duration_outlier?, :boolean
+    add_column :instance_entries, :xp_outlier?, :boolean
+    add_column :instance_entries, :notes, :string
+  end
+end
