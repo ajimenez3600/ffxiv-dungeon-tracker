57145cfdda6508b94f2d13c1775c2fabd94e8a39
Andrew <blakexiaolong36@gmail.com>
2021-04-16 01:37:14 -0400

    fix dumb mis-typing

--- db/migrate/20210415230434_add_form_fields_to_instance_entry.rb
+++ db/migrate/20210415230434_add_form_fields_to_instance_entry.rb
@@ -3,19 +3,19 @@ class AddFormFieldsToInstanceEntry < ActiveRecord::Migration[6.1]
     add_column :instance_entries, :start_time, :datetime
     add_column :instance_entries, :roulette_name, :string
     add_column :instance_entries, :job_name, :string
-    add_column :instance_entries, :start_level, :number
-    add_column :instance_entries, :start_xp, :number
+    add_column :instance_entries, :start_level, :integer
+    add_column :instance_entries, :start_xp, :integer
     add_column :instance_entries, :queue_pop_time, :datetime
     add_column :instance_entries, :instance_selection, :string
     add_column :instance_entries, :finish_time, :datetime
-    add_column :instance_entries, :finish_level, :number
-    add_column :instance_entries, :finish_xp, :number
-    add_column :instance_entries, :xp_bonus, :number
-    add_column :instance_entries, :roulette_bonus, :number
-    add_column :instance_entries, :new_player_bonus, :number
-    add_column :instance_entries, :role_in_need_bonus, :number
-    add_column :instance_entries, :other_bonus, :number
-    add_column :instance_entries, :commends, :number
+    add_column :instance_entries, :finish_level, :integer
+    add_column :instance_entries, :finish_xp, :integer
+    add_column :instance_entries, :xp_bonus, :integer
+    add_column :instance_entries, :roulette_bonus, :integer
+    add_column :instance_entries, :new_player_bonus, :integer
+    add_column :instance_entries, :role_in_need_bonus, :integer
+    add_column :instance_entries, :other_bonus, :integer
+    add_column :instance_entries, :commends, :integer
     add_column :instance_entries, :queue_outlier?, :boolean
     add_column :instance_entries, :duration_outlier?, :boolean
     add_column :instance_entries, :xp_outlier?, :boolean
