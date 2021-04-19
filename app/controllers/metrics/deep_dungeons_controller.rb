fd104a2f50fb324a9cb234d719cdf1eaeadc93ef
ajimenez3600 <blakexiaolong36@gmail.com>
2021-04-18 20:13:58 -0400

    don't try and set job from params

--- app/controllers/metrics/deep_dungeons_controller.rb
+++ app/controllers/metrics/deep_dungeons_controller.rb
@@ -1,5 +1,4 @@
 class Metrics::DeepDungeonsController < ApplicationController
   def index
-    @instance_entries = InstanceEntries.all.select { e| e.instance.instance_type == 'Deep Dungeon' }
   end
 end
--- app/controllers/metrics/trials_controller.rb
+++ app/controllers/metrics/trials_controller.rb
@@ -1,5 +1,4 @@
 class Metrics::TrialsController < ApplicationController
   def index
-    @instance_entries = InstanceEntries.all.select { e| e.instance.instance_type == 'Trial' }
   end
 end
