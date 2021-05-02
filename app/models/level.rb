29c99f88289c04c48a5312655eb59a555116af24
ajimenez3600 <blakexiaolong36@gmail.com>
2021-05-02 00:32:33 -0400

    add defaults and validation

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,5 +1,8 @@
 class Level < ApplicationRecord
-  def self.total_xp(level, xp)
+  def self.total_xp(level, xp = 0)
     Level.find_by_number(level).total_xp + xp
+  end
+  def get_level(xp)
+    Level.where(total_xp: xp..).map(&:number).min
   end
 end
