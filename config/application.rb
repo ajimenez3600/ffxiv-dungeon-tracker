a169679fa0bfb068aed19bf835294b0f7fc705bf
Andrew Jimenez <ajimenez3600@gmail.com>
2022-01-14 12:10:17 -0500

    change it in the config as well

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module FfxivDungeonTracker
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.min_patch_divider = 6.0
-    config.patch_divider = 6.0
+    config.min_patch_number = 6.0
+    config.patch_number = 6.0
   end
 end
